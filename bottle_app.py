72a45307131444df0c502661193d79cc7c844196
Emma Indal <emma.indahl@gmail.com>
2017-05-03 10:44:53 +0200

    no message

--- bottle_app.py
+++ bottle_app.py
@@ -6,6 +6,27 @@ import os, sys
 import codecs
 import json
 
+
+import psycopg2
+
+# connect to database
+conn = psycopg2.connect(dbname="db_project", host="localhost", port="5432")
+
+# create a cursor
+cursor = conn.cursor()
+
+
+# execute SQL statement
+cursor.execute("select * from artikel")
+
+# get the resultset as a tuple
+result = cursor.fetchall()
+
+# iterate through resultset
+for artikel in result:
+	print (artikel)
+
+
 def get_articles():
     # Listar alla filer som finns i storage
     files = os.listdir("storage")
